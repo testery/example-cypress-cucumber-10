aecf868bd212f53dc5732b4061489e94c137b493
jheffernan <justinth9543@gmail.com>
2022-07-22 14:37:34 -0500

    fix: Support file not needed

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const {defineConfig} = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: "**/*.feature"
+    specPattern: "**/*.feature",
+    supportFile: false
   }
 })

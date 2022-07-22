c02c290758024a1fc57175ad2e58b1140a230a4c
jheffernan <justinth9543@gmail.com>
2022-07-22 17:16:40 -0500

    fix: Migrated to new way of handling plugins

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,11 @@
 const {defineConfig} = require("cypress");
+const cucumber = require('cypress-cucumber-preprocessor').default
 
 module.exports = defineConfig({
   e2e: {
+    setupNodeEvents(on, config) {
+      on('file:preprocessor', cucumber())
+    },
     specPattern: "**/*.feature",
     supportFile: false
   }

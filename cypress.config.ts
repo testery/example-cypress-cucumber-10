e3e9616e9e67cc547b394c2c42e7b9160d55bb51
Will Gunn <will.gunn@testery.io>
2024-05-16 10:38:04 -0500

    try function outside e2e

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,23 +3,25 @@ import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
 import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
 import { createEsbuildPlugin } from "@badeball/cypress-cucumber-preprocessor/esbuild";
 
+async function setupNodeEvents(on: Cypress.PluginEvents,config: Cypress.PluginConfigOptions): Promise<Cypress.PluginConfigOptions> {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await addCucumberPreprocessorPlugin(on, config);
+
+  on(
+    "file:preprocessor",
+    createBundler({
+      plugins: [createEsbuildPlugin(config)],
+    })
+  );
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
+
 export default defineConfig({
   e2e: {
     specPattern: "**/*.feature",
     supportFile: false,
-    async setupNodeEvents(on: Cypress.PluginEvents,config: Cypress.PluginConfigOptions): Promise<Cypress.PluginConfigOptions> {
-      // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
-      await addCucumberPreprocessorPlugin(on, config);
-
-      on(
-        "file:preprocessor",
-        createBundler({
-          plugins: [createEsbuildPlugin(config)],
-        })
-      );
-
-      // Make sure to return the config object as it might have been modified by the plugin.
-      return config;
-    },
+    setupNodeEvents
   }
 });
